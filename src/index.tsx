45c5a0712f5a89bd9eb750a23fc9bbf9264000d6
iTNTPiston <zhaoyizhu2000@gmail.com>
2023-07-21 23:45:37 -0700

    simplify index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import { Info24Filled } from "@fluentui/react-icons";
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-
-function App() {
-    return <Info24Filled />;
-}
-
-root.render(<App />);
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<Info24Filled />);
